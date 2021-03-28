9a68199b41df6b61dd2629d3e3fe4c66ae3f17ea
radikzoom <54982901+radikzoom@users.noreply.github.com>
2021-03-29 02:11:09 +0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,1 +1,33 @@
+from random import randint
+n = 5 
+m = 5 
+
+a = []
+
+for i in range(n):
+    a.append([])
+    for j in range(m):
+        a[i].append(randint(-5, 5))
+for i in range(n):
+    print("")
+    for j in range(m):
+        print(a[i][j], end=' ')
+
+
+b = 0
+for i in range(n):
+    for j in range(m):
+        if a[i][j] > 0:
+            b += 1
+
+c = 0
+for i in range(n):
+    for j in range(m):
+        if a[i][j] < 0:
+            c += 1
+
+
+print('\n')
+print('Колл положительных', b)
+print('Колл отрицательных', c)
 
