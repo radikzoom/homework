b1a7bacca54e813905f377713510ea3842d392aa
radikzoom <54982901+radikzoom@users.noreply.github.com>
2021-03-31 22:08:25 +0300

    Update matrix_maxEl_index.py

--- matrix_maxEl_index.py
+++ matrix_maxEl_index.py
@@ -1,4 +1,5 @@
 from random import randint
+
 n = 3
 m = 5
 k = 7
@@ -9,7 +10,14 @@ for n1 in range(n):
     for m1 in range(m):
         t[n1].append([])
         for k1 in range(k):
-            t[n1][m1].append(randint(-20,20))
+            t[n1][m1].append(randint(-20, 20))
+#
+for n1 in range(n): # обрати внимание как распечатал матрицу
+    print(' ')
+    for m1 in range(m):
+        print(' ')
+        for k1 in range(k):
+            print(t[n1][m1][k1], end=' ')
 
 max_el = -100
 for n2 in range(n):
@@ -24,9 +32,8 @@ for n3 in range(n):
         ind_x[n3].append([])
         for k3 in range(k):
             if t[n3][m3][k3] == max_el:
-                print('Индекс макс.значений матрицы:', n3, m3, k3)
+                print('\nИндекс макс.значений матрицы:', n3, m3, k3)
 
-print(t)
 print(max_el)
 
 
